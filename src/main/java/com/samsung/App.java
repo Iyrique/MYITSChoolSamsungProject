521b5f8e4c958ea2a35983ed755c275a8e7e3510
remoppou <voronezhskij_n_m@sc.vsu.ru>
2022-05-14 14:31:30 +0300

    H2, liquibase

--- src/main/java/com/samsung/App.java
+++ src/main/java/com/samsung/App.java
@@ -1,9 +1,15 @@
 package com.samsung;
 
 
+import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.sql.SQLException;
+
+// url h2 консоли: http://localhost:8081/h2-console
+// url базы: jdbc:h2:mem:testdb
 
 @SpringBootApplication
 public class App {
@@ -11,6 +17,13 @@ public class App {
     public static void main(String[] args) {
 
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+
+        try {
+            //Консоль для визуализации бд в браузере
+            Console.main(args);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
